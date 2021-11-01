c869ed6d7da22a2109426211021222b29f08383f
Aleksey Saprunov <workernight@yandex.ru>
2021-11-01 22:24:54 +0400

    Activity main and options menu

--- app/src/main/java/com/alexsp0/nicefilmapp/MainActivity.kt
+++ app/src/main/java/com/alexsp0/nicefilmapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.alexsp0.nicefilmapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_main_search -> {
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
