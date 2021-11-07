e12be301537921a80f4d870bb71c129851bce26d
Alexey S <workernight@yandex.ru>
2021-11-07 18:24:25 +0400

    Convert Film class to kotlin

---
+++ app/src/main/java/com/alexsp0/nicefilmapp/ui/main/CurrentFilmFragment.kt
@@ -0,0 +1,28 @@
+package com.alexsp0.nicefilmapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alexsp0.nicefilmapp.R
+
+class CurrentFilmFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_current_film, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =  CurrentFilmFragment()
+    }
+}
---
+++ app/src/main/java/com/alexsp0/nicefilmapp/utils/Film.kt
@@ -0,0 +1,3 @@
+package com.alexsp0.nicefilmapp.utils
+
+class Film(var name: String, var genre: String, var cover: Int)
