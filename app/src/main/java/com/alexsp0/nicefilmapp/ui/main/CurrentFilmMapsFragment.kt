016355f04b6962ac672b9e9aaf8c07ffe8e68091
Alex <workernight@yandex.ru>
2021-12-16 19:16:47 +0400

    Move camera to film prodaction country

--- app/src/main/java/com/alexsp0/nicefilmapp/ui/main/CurrentFilmMapsFragment.kt
+++ app/src/main/java/com/alexsp0/nicefilmapp/ui/main/CurrentFilmMapsFragment.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.LocationManager
 import android.net.Uri
 import androidx.fragment.app.Fragment
@@ -31,18 +32,16 @@ import com.google.android.gms.maps.model.MarkerOptions
 class CurrentFilmMapsFragment(country : String) : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val act = activity as MainActivity
+        val geoCoder = Geocoder(act.applicationContext)
+        val address = geoCoder.getFromLocationName(country,1)
+        if (address != null) {
+            val location = address[0]
+            val pos = LatLng(location.latitude, location.longitude)
+            googleMap.addMarker(MarkerOptions().position(pos).title("Production country is here!"))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(pos))
+        }
+
     }
 
     override fun onCreateView(
